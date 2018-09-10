dd3fa7a6925f168cd33a628899f76d3270fd5b5d
최혁재 <>
2018-09-10 18:29:35 +0900

    ColorListActivity에 +버튼과 색깔, 수량 입력하는 Form을 겹친 레이아웃을 생성

--- app/src/main/java/com/example/hyeok/eventbusexample/ColorListActivity.java
+++ app/src/main/java/com/example/hyeok/eventbusexample/ColorListActivity.java
@@ -2,6 +2,7 @@ package com.example.hyeok.eventbusexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ColorListActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ColorListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_list);
+    }
+
+    public void addColorAmount(View view){
+
     }
 }
--- app/src/main/java/com/example/hyeok/eventbusexample/MainActivity.java
+++ app/src/main/java/com/example/hyeok/eventbusexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hyeok.eventbusexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -65,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, ColorListActivity.class);
+                startActivity(intent);
             }
         });
     }
